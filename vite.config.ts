0998acd9577af5f7c8b6ce06ec98896fac5f8fc5
Alexander. <siansololevelderly@gmail.com>
2025-05-09 16:56:24 +0300

    1

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.BASE_URL || '/', 
+  // base: process.env.BASE_URL || '/', 
+  base: '/design-app/',
   server: {
     proxy: {
       '/api': {
